78fb726c0a303ced1cba3a1faadcb5cfece4f1d9
lhawi <lukmanhawi@gmail.com>
2023-06-11 21:09:50 +0700

    Implemented user repository with Save method to persist user data into the database

--- main.go
+++ main.go
@@ -3,38 +3,12 @@ package main
 import (
 	"bwastartup/user"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Connection to database is good")
-
-	// var users []user.User
-	// db.Find(&users)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// 	fmt.Println("============")
-	// }
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context){
 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -42,8 +16,11 @@ func handler(c *gin.Context){
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "Test Simapan",
+	}
 
-	c.JSON(http.StatusOK, users)
+	userRepository.Save(user)
+
 }
